aa93a1500c1693bfdddc0c28461fc4bc8e3c2350
nikhil21174 <108423082+nikhil21174@users.noreply.github.com>
2024-07-07 02:49:48 +0530

    Update table_creation.sql

--- table_creation.sql
+++ table_creation.sql
@@ -44,7 +44,7 @@ CREATE TABLE Cart (
 CREATE TABLE Authentication (
   Customer_ID varchar(50),
   Email varchar(100),
-  Customer_Password varbinary(255),
+  Customer_Password varcar(100),
   PRIMARY KEY (Customer_ID)
 );
 
